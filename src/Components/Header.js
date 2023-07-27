d4e2c9a5014dd4c263530bb885fe17570913cd22
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-07-27 08:35:52 +0000

    Fixed element alignment and added custom scrollbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,13 +10,12 @@ export default function Header() {
   <header class="header">
     <div class="mid">
       <ul class="navbar">
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About Us</Link></li>
-        <li><Link to='/resources'>Resources</Link></li>
-        <li><Link to='/faqs'>FAQ's</Link></li>
+        <li><Link to="/societies">Societies</Link></li>
         <li><Link to="/contribute">Contribute</Link></li>
-        <li><Link to="/societies">Societies</Link>
-        </li>
+        <li><Link to='/faqs'>FAQ's</Link></li>
+        <li><Link to='/resources'>Resources</Link></li>
+        <li><Link to='/about'>About Us</Link></li>
+        <li><Link to='/'>Home</Link></li>
       </ul>
     </div>
   </header>
